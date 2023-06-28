698d92115a95ef530029b90d4f668833a27f6e83
Lawmsangi <ddarkphoenixx22@gmail.com>
2023-06-28 15:55:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,116 +15,87 @@ function closedMenu() {
   document.body.style.overflow = 'scroll';
 }
 close.addEventListener('click', closedMenu);
-
 menu.addEventListener('click', closedMenu);
 
 // product functions
 const productCard = document.getElementById('section-product');
 const productHeader = document.createElement('div');
-const productContainer = document.createElement('div');
-const productOne = document.createElement('div');
-const productTwo = document.createElement('div');
-const productThree = document.createElement('div');
-const productFour = document.createElement('div');
-const productFive = document.createElement('div');
-const productSix = document.createElement('div');
-const showMore = document.createElement('div');
-
-productHeader.className = 'product-header';
-productContainer.className = 'product-container';
-productOne.className = 'product-content';
-productTwo.className = 'product-content';
-productThree.className = 'product-content';
-productFour.className = 'product-content';
-productFive.className = 'product-content';
-productSix.className = 'product-content';
-showMore.className = 'show-more';
 
 productHeader.innerHTML = `<h2>Main Products</h2>
                             <div class="line"></div>`;
 productCard.appendChild(productHeader);
 
-productOne.innerHTML = `
-      <div class="product-image">
-        <img src="images/1.jpeg" alt="product-image">
-      </div>
-    <div class="product-details">
-        <h4>Bubu Dudu Sleeping Hoodie</h4>
-        <span>&#8377 999</span> &#8377 799
-        <hr class="line-black">
-        <img src="images/truck.svg" alt="truck" style="color:grey">
-        Standard shipping in 10 working days
-    </div>`;
+const productContainer = document.createElement('div');
 
-productTwo.innerHTML = `
-      <div class="product-image">
-        <img src="images/2.jpeg" alt="product-image">
-      </div>
-    <div class="product-details">
-        <h4>Bubu Dudu King Hoodie</h4>
-        <span>&#8377 999</span> &#8377 799
-        <hr class="line-black">
-        <img src="images/truck.svg" alt="truck" style="color:grey">
-        Standard shipping in 10 working days
-    </div>`;
+const products = [
+  {
+    image: 'images/1.jpeg',
+    title: 'Bubu Dudu Sleeping Hoodies',
+    price: '&#8377 999',
+    img: 'images/truck.svg',
+  },
+  {
+    image: 'images/2.jpeg',
+    title: 'Bubu Dudu Sleeping Hoodies',
+    price: '&#8377 999',
+    img: 'images/truck.svg',
+  },
+  {
+    image: 'images/3.jpeg',
+    title: 'Bubu Dudu Sleeping Hoodies',
+    price: '&#8377 999',
+    img: 'images/truck.svg',
+  },
+  {
+    image: 'images/4.jpeg',
+    title: 'Bubu Dudu Sleeping Hoodies',
+    price: '&#8377 999',
+    img: 'images/truck.svg',
+  },
+  {
+    image: 'images/5.jpeg',
+    title: 'Bubu Dudu Sleeping Hoodies',
+    price: '&#8377 999',
+    img: 'images/truck.svg',
+  },
+  {
+    image: 'images/6.jpeg',
+    title: 'Bubu Dudu Sleeping Hoodies',
+    price: '&#8377 999',
+    img: 'images/truck.svg',
+  },
+];
 
-productThree.innerHTML = `
-      <div class="product-image">
-        <img src="images/3.jpeg" alt="product-image">
-      </div>
-    <div class="product-details">
-        <h4>Bubu Dudu Sleeping Hoodie</h4>
-        <span>&#8377 999</span> &#8377 799
-        <hr class="line-black">
-        <img src="images/truck.svg" alt="truck" style="color:grey">
-        Standard shipping in 10 working days
-    </div>`;
+products.forEach((product, index) => {
+  const {
+    image, title, price, img,
+  } = products[index];
+  const productContent = document.createElement('div');
+  productContent.className = 'product-content';
+  productContent.innerHTML = `
+                                <div class="product-image">
+                                    <img src=${image} alt="product-image">
+                                </div>
+                                <div class="product-details">
+                                    <h4>${title}</h4>
+                                    <span>${price}</span> &#8377 799
+                                    <hr class="line-black">
+                                    <img src=${img} alt="truck" style="color:grey">
+                                    Standard shipping in 10 working days
+                                </div>`;
 
-productFour.innerHTML = `
-      <div class="product-image">
-        <img src="images/4.jpeg" alt="product-image">
-      </div>
-    <div class="product-details">
-        <h4>Bubu Dudu Happy Birthday White Cushion</h4>
-        <span>&#8377 499</span> &#8377 799
-        <hr class="line-black">
-        <img src="images/truck.svg" alt="truck" style="color:grey">
-        Standard shipping in 10 working days
-    </div>`;
+  productContainer.appendChild(productContent);
+});
 
-productFive.innerHTML = `
-      <div class="product-image">
-        <img src="images/5.jpeg" alt="product-image">
-      </div>
-    <div class="product-details">
-        <h4>Happy Anniversary Bubu Dudu Magic Mug</h4>
-        <span>&#8377 499</span> &#8377 799
-        <hr class="line-black">
-        <img src="images/truck.svg" alt="truck" style="color:grey">
-        Standard shipping in 10 working days
-    </div>`;
+const showMore = document.createElement('div');
 
-productSix.innerHTML = `
-      <div class="product-image">
-        <img src="images/6.jpeg" alt="product-image">
-      </div>
-    <div class="product-details">
-        <h4>Bubu Dudu(Bear Panda) Soft Toys/Plushies/Teddy</h4>
-        <span>&#8377 1800</span> &#8377 799
-        <hr class="line-black">
-        <img src="images/truck.svg" alt="truck" style="color:grey">
-        Standard shipping in 10 working days
-    </div>`;
+productHeader.className = 'product-header';
+productContainer.className = 'product-container';
+showMore.className = 'show-more';
 
 showMore.innerHTML = `<span>More</span>
                         <img src="images/arrow-down.png" alt="arrow-down">`;
 
-productContainer.appendChild(productOne);
-productContainer.appendChild(productTwo);
-productContainer.appendChild(productThree);
-productContainer.appendChild(productFour);
-productContainer.appendChild(productFive);
-productContainer.appendChild(productSix);
 productCard.appendChild(productContainer);
 
 showMore.addEventListener('click', () => {
